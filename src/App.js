1106e4987a45a82aef0d36a4715c2f035a72b9c7
Steven <stevenklavins94@gmail.com>
2020-07-06 22:06:59 +0100

    chore: called game component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Game from './Components/Game'
 
 function App() {
   return (
     <div className="App">
       <h1>Tic Tac Toe</h1>
+      <Game />
     </div>
   )
 }
