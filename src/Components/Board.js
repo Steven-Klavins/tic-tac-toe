00b2d480318a7bd4457982c3769ff996dfe7e5bb
Steven <stevenklavins94@gmail.com>
2020-07-07 16:46:00 +0100

    feat: player X can now add place X on board

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,8 +3,26 @@ import Square from './Square'
 import '../Components/Board.css'
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({ squares: squares })
+  }
+
   renderSquare(i) {
-    return <Square />
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render() {
